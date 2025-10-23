8cf2630c15f3eddf1b4b3d2df90516898e75f62d
Bruno Rocha <brnrocha@gmail.com>
2025-10-23 09:02:44 -0400

    Add files via upload
    Initial commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿//Screen Sound
+string mensagemDeBoasVindas = "Boas Vindas ao Screen Sound";
+string curso = "C#";
+string nome = "Bruno";
+string sobrenome = "Rocha";
+Console.WriteLine(mensagemDeBoasVindas);
+Console.WriteLine(curso);
+Console.WriteLine(nome + sobrenome);
+Console.WriteLine("Nome dos instrutores");
